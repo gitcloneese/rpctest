739c0af1842abb33a4df46492ec6ca7c77ef9ede
panliangliang <panll@blizzmi.com>
2021-03-01 10:52:55 +0800

    添加comment

--- server.go
+++ server.go
@@ -7,7 +7,20 @@ import (
 	"net/http"
 	"net/rpc"
 )
+/*
+	Doc  Addr
+	
+ 	http://docscn.studygolang.com/pkg/net/rpc/	
+	
+- the method's type is exported.
+- the method is exported.
+- the method has two arguments, both exported (or builtin) types.
+- the method's second argument is a pointer.
+- the method has return type error.
 
+   func (t *T) MethodName(argType T1, replyType *T2) error
+
+*/
 
 type Panda int
 
